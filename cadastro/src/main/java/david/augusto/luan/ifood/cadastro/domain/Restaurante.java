3d5518020b7fb352e87d698426b9eff88a8ebb10
luan-alencar <luanalencar134@gmail.com>
2023-03-21 10:29:57 -0300

    first commit

---
+++ cadastro/src/main/java/david/augusto/luan/ifood/cadastro/domain/Restaurante.java
@@ -0,0 +1,41 @@
+package david.augusto.luan.ifood.cadastro.domain;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import david.augusto.luan.ifood.cadastro.domain.Localizacao;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+@Entity
+@Table(name = "restaurante")
+public class Restaurante extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String proprietario;
+
+    public String cnpj;
+
+    public String nome;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    public Localizacao localizacao;
+
+    @CreationTimestamp
+    public Date dataCriacao;
+
+    @UpdateTimestamp
+    public Date dataAtualizacao;
+}
---
+++ cadastro/src/test/java/david/augusto/luan/ifood/pedido/cadastro/PedidoResourceIT.java
@@ -0,0 +1,8 @@
+package david.augusto.luan.ifood.pedido.cadastro;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class PedidoResourceIT extends PedidoResourceTest {
+    // Execute the same tests but in packaged mode.
+}
