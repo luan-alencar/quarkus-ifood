9ee86cf16ac3654cd857aa1f635b3b8153ddc7e6
luan-alencar <luanalencar134@gmail.com>
2023-03-22 19:32:04 -0300

    CRUD - restaurante

--- cadastro/src/main/java/david/augusto/luan/ifood/cadastro/domain/RestauranteResource.java
+++ cadastro/src/main/java/david/augusto/luan/ifood/cadastro/domain/RestauranteResource.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.Optional;
 
 @ApplicationScoped
 @Path("/api/restaurantes")
@@ -23,5 +24,31 @@ public class RestauranteResource {
     @Transactional
     public void salvar(Restaurante dto) {
         Restaurante.persist(dto);
+    }
+
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public void atualizar(@PathParam("id") Long id, Restaurante dto) {
+        Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(id);
+        if (restauranteOp.isEmpty()) {
+            throw new NotFoundException();
+        }
+        Restaurante restaurante = restauranteOp.get();
+
+        restaurante.nome = dto.nome;
+        restaurante.persist();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public void delete(@PathParam("id") Long id) {
+        Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(id);
+
+        restauranteOp.ifPresentOrElse(Restaurante::delete, () -> {
+            throw new NotFoundException();
+        });
     }
 }
